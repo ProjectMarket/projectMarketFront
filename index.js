82cbb96c62e9f5fed1b8b20af337dfeb6430f27b
Hannibal974 <romain.poussin@ynov.com>
2016-11-16 16:49:34 +0100

    modification index.js pour load script in index.html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var express = require('express'),
 	app = express(),
 	port = process.env.PORT || 8080;
 
+app.use(express.static(__dirname + '/'));
+
 app.get('/', function(req, res) {
 	res.sendFile(__dirname+'/index.html');
 });
