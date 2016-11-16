d0e8a8f3985b5e49073f55a4e0c94970ff2041ee
Hannibal974 <romain.poussin@ynov.com>
2016-11-16 15:38:18 +0100

    modification package.json + index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-document.location.href="index.html";
+var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 8080;
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname+'/index.html');
+});
+
+app.listen(port, function() {
+
+});
